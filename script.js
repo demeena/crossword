86e94b563d7230830015f6736c1146fd1199755e
Aleksandra Kalashnikova <85189965+demeena@users.noreply.github.com>
2022-07-11 22:31:59 +0300

    загружаю файлы сайта

---
+++ script.js
@@ -0,0 +1,237 @@
+
+let sq= document.querySelector('#raz')
+let sr= document.querySelector('#dva')
+let sd= document.querySelector('#tri')
+let sf= document.querySelector('#ch')
+let sg= document.querySelector('#py')
+let sj= document.querySelector('#shr')
+let sl= document.querySelector('#sem')
+let se= document.querySelector('#vos')
+
+
+let v= document.querySelector('#v')
+let e= document.querySelector('#e')
+let n= document.querySelector('#n')
+let i= document.querySelector('#i')
+let k= document.querySelector('#k')
+
+let w= document.querySelector('#w')
+let uy= document.querySelector('#uy')
+let y= document.querySelector('#y')
+
+let b= document.querySelector('#b')
+let yyy= document.querySelector('#yyy')
+let r= document.querySelector('#r')
+let ya= document.querySelector('#ya')
+
+let n1 = document.querySelector('#t1')
+let n2= document.querySelector('#t2')
+let n3= document.querySelector('#t3')
+let n4= document.querySelector('#t4')
+let n5= document.querySelector('#t5')
+let n6= document.querySelector('#t6')
+let n7= document.querySelector('#t7')
+
+let x1 = document.querySelector('#x1')
+let x2= document.querySelector('#x2')
+let x3= document.querySelector('#x3')
+
+let v1 = document.querySelector('#v1')
+let v2= document.querySelector('#v2')
+let v3= document.querySelector('#v3')
+let v4 = document.querySelector('#v4')
+let v5= document.querySelector('#v5')
+
+
+let h1 = document.querySelector('#h1')
+let h2= document.querySelector('#h2')
+let h3= document.querySelector('#h3')
+let h4 = document.querySelector('#h4')
+let h5= document.querySelector('#h5')
+let h6 = document.querySelector('#h6')
+let h7= document.querySelector('#h7')
+
+
+let z1 = document.querySelector('#z1')
+let z2= document.querySelector('#z2')
+let z3= document.querySelector('#z3')
+let z4 = document.querySelector('#z4')
+let z5= document.querySelector('#z5')
+
+let q1 = document.querySelector('#q1')
+let q2= document.querySelector('#q2')
+let q3= document.querySelector('#q3')
+
+function f(){
+   
+        if(sq.value == "с") {
+          sq.style.backgroundColor='green'
+        }
+        if(sr.value == "в") {
+            sr.style.backgroundColor='yellow'
+          }
+          if(sd.value == "а") {
+            sd.style.backgroundColor='green'
+          }
+          if(sf.value == "с") {
+            sf.style.backgroundColor='green'
+          }
+          if(sg.value == "т") {
+            sg.style.backgroundColor='green'
+          }
+          if(sj.value == "и") {
+            sj.style.backgroundColor='green'
+          }
+          if(sl.value == "к") {
+            sl.style.backgroundColor='green'
+          }
+          if(se.value == "а") {
+            se.style.backgroundColor='green'
+          }
+          if(v.value == "в") {
+            v.style.backgroundColor='green'
+          }
+          if(e.value == "е") {
+            e.style.backgroundColor='green'
+          }
+          if(n.value == "н") {
+            n.style.backgroundColor='green'
+          }
+          if(i.value == "и") {
+            i.style.backgroundColor='yellow'
+          }
+          if(k.value == "к") {
+            k.style.backgroundColor='green'
+          }
+
+          if(w.value == "в") {
+            w.style.backgroundColor='green'
+          }
+          if(uy.value == "и") {
+            uy.style.backgroundColor='green'
+          }
+          if(y.value == "т") {
+            y.style.backgroundColor='yellow'
+          }
+          
+          if(b.value == "б") {
+            b.style.backgroundColor='green'
+          }
+
+          if(yyy.value == "у") {
+            yyy.style.backgroundColor='green'
+          }
+          if(r.value == "р") {
+            r.style.backgroundColor='green'
+          }
+          if(ya.value == "я") {
+            ya.style.backgroundColor='yellow'
+          }
+          if(n1.value == "п") {
+            n1.style.backgroundColor='green'
+          }
+          if(n2.value == "о") {
+              n2.style.backgroundColor='green'
+            }if(n3.value == "с") {
+              n3.style.backgroundColor='green'
+            }if(n4.value == "т") {
+              n4.style.backgroundColor='green'
+            }if(n5.value == "е") {
+              n5.style.backgroundColor='green'
+            }if(n6.value == "л") {
+              n6.style.backgroundColor='yellow'
+            }if(n7.value == "ь") {
+              n7.style.backgroundColor='green'
+            }
+            if(x1.value == "л") {
+                x1.style.backgroundColor='green'
+              }
+              if(x2.value == "у") {
+                  x2.style.backgroundColor='yellow'
+                }if(x3.value == "к") {
+                  x3.style.backgroundColor='green'
+                }
+                if(v1.value == "ч") {
+                    v1.style.backgroundColor='yellow'
+                  }
+                  if(v2.value == "а") {
+                      v2.style.backgroundColor='green'
+                    }if(v3.value == "ш") {
+                      v3.style.backgroundColor='green'
+                    }if(v4.value == "к") {
+                        v4.style.backgroundColor='green'
+                      }
+                      if(v5.value == "а") {
+                          v5.style.backgroundColor='green'
+                        }
+
+                        if(h1.value == "ш") {
+                            h1.style.backgroundColor='green'
+                          }
+                          if(h2.value == "е") {
+                              h2.style.backgroundColor='green'
+                            }if(h3.value == "р") {
+                              h3.style.backgroundColor='green'
+                            }if(h4.value == "ш") {
+                                h4.style.backgroundColor='yellow'
+                              }
+                              if(h5.value == "е") {
+                                  h5.style.backgroundColor='green'
+                                }
+                                if(h6.value == "н") {
+                                    h6.style.backgroundColor='green'
+                                  }
+                                  if(h7.value == "ь") {
+                                      h7.style.backgroundColor='green'
+                                    }
+
+
+
+
+
+                                    if(z1.value == "и") {
+                                        z1.style.backgroundColor='green'
+                                      }
+                                      if(z2.value == "д") {
+                                          z2.style.backgroundColor='green'
+                                        }if(z3.value == "и") {
+                                          z3.style.backgroundColor='yellow'
+                                        }if(z4.value == "о") {
+                                            z4.style.backgroundColor='green'
+                                          }
+                                          if(z5.value == "т") {
+                                              z5.style.backgroundColor='green'
+                                            }
+
+
+
+                                            if(q1.value == "ч") {
+                                                q1.style.backgroundColor='green'
+                                              }
+                                              if(q2.value == "а") {
+                                                  q2.style.backgroundColor='green'
+                                                }if(q3.value == "й") {
+                                                  q3.style.backgroundColor='yellow'
+                                                }
+                            }
+                           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+   
